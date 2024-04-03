00a268a70a0faeeefae32f88dc2283922adb595c
durga <durga.annepu@gmail.com>
2024-04-03 04:12:43 +0000

    browser-history.reactjs

---
+++ src/components/index.js
@@ -0,0 +1,33 @@
+import './index.css'
+const HistoryItem = props => {
+  const {searchDetails,delteUser} = props
+  const {id, timeAccessed, logoUrl, title, domainUrl} = searchDetails
+  const onDelete=(){
+    delteUser(id)
+  }
+  return (
+    <li className="list-container">
+      <div className="text-container">
+        <p className="description">{timeAccessed}</p>
+        <img
+          src={logoUrl}
+          className="image"
+          alt="app logo"
+        />
+        <p className="description">{title}</p>
+        <p className="description">{domainUrl}</p>
+      </div>
+      <div className="img-container">
+        <button className="btn" data-testid="delete" onClick={onDelete}>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/delete-img.png"
+            className="image"
+            alt="delete"
+          />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default HistoryItem
